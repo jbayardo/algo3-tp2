7b35913a34d37d9b7edc16b52171a322cc0f76c5
Izikiel <ezequiel.gambaccini@gmail.com>
2015-09-28 11:55:39 -0300

    Refactoreo de la funcion para generar casos, ahora es genérica.

--- generate.py
+++ generate.py
@@ -23,21 +23,6 @@ def worst_case_1(top_floor):
     return str(top_floor) + "\n" + "; ".join(data)
 
 
-def generate_ex1(max_cases):
-    best_file = EX_DIR + EX_FILE_TEMPLATE % (1, BEST)
-    worst_file = EX_DIR + EX_FILE_TEMPLATE % (1, WORST)
-
-    best_output = []
-    worst_output = []
-
-    for x in xrange(1, max_cases):
-        best_output.append(best_case_1(x))
-        worst_output.append(worst_case_1(x))
-
-    write_ex_file(best_file, "\n".join(best_output))
-    write_ex_file(worst_file, "\n".join(worst_output))
-
-
 ####################################
 ############# Exercise 2 ###########
 ####################################
@@ -48,6 +33,7 @@ def best_case_2(top):
         cases.append("%d %d\n0 0 %d 0" % (top, l, top))
     return "\n".join(cases)
 
+
 def worst_case_2(top):
     cases = defaultdict(list)
     for l in xrange(2, max(top+1, 3)):
@@ -55,21 +41,6 @@ def worst_case_2(top):
             portal_x = ((floor+1) % 2) * l
             cases["%d %d\n" % (top, l)].append("%d %d %d %d" % (floor, portal_x, floor + 1, portal_x))
     return "\n".join([k + "; ".join(v) for k, v in cases.iteritems()])
-
-
-def generate_ex2(max_cases):
-    best_file = EX_DIR + EX_FILE_TEMPLATE % (2, BEST)
-    worst_file = EX_DIR + EX_FILE_TEMPLATE % (2, WORST)
-
-    best_output = []
-    worst_output = []
-
-    for x in xrange(1, max_cases):
-        best_output.append(best_case_1(x))
-        worst_output.append(worst_case_1(x))
-
-    write_ex_file(best_file, "\n".join(best_output))
-    write_ex_file(worst_file, "\n".join(worst_output))
 
 
 ####################################
@@ -87,6 +58,29 @@ def worst_case_3(top):
 ####################################
 ############# General ##############
 ####################################
+best_case = {}
+best_case[1] = best_case_1
+best_case[2] = best_case_2
+best_case[3] = best_case_3
+
+worst_case = {}
+worst_case[1] = worst_case_1
+worst_case[2] = worst_case_2
+worst_case[3] = worst_case_3
+
+def generate_ex(ex, max_cases):
+    best_file = EX_DIR + EX_FILE_TEMPLATE % (ex, BEST)
+    worst_file = EX_DIR + EX_FILE_TEMPLATE % (ex, WORST)
+
+    best_output = []
+    worst_output = []
+
+    for x in xrange(1, max_cases):
+        best_output.append(best_case[ex](x))
+        worst_output.append(worst_case[ex](x))
+
+    write_ex_file(best_file, "\n".join(best_output))
+    write_ex_file(worst_file, "\n".join(worst_output))
 
 
 def write_ex_file(path, data):
@@ -99,5 +93,5 @@ if __name__ == '__main__':
     #     print "Numero Problema a generar"
     if not os.path.exists(EX_DIR):
         os.makedirs(EX_DIR)
-    generate_ex1(100)
-    generate_ex2(100)
+    generate_ex(1, 100)
+    generate_ex(2, 100)
